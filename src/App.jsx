f93a677d32e32d89f890f412b6913632d654d5cd
SaadAli2004 <mohammadsaadali16@gmail.com>
2025-09-13 14:38:46 +0500

    Added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return <></>;
+  const key = import.meta.env.API_KEY;
+  const database = async() => {
+    const url = `https://api.rawg.io/api/games?key=${key}&dates=2019-09-01,2019-09-30&platforms=18,1,7`
+    await axios.get(url)
+    .then(response => {console.log(response.data)})
+    .catch(error => {console.error(error)})
+  
+
+
+  }
+  useEffect(() => {database()},[])
+  return <>
+  </>;
 }
 
 export default App;
