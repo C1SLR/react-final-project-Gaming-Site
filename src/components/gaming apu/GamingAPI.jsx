7ccc12da757a2948f95770e6b4f0696974558e32
C1SLR <umarfarooq7282003@gmail.com>
2025-09-13 14:17:55 +0500

    Axios installation

---
+++ src/components/gaming apu/GamingAPI.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GamingAPI() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GamingAPI
---
+++ src/components/gaming card/GamingCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GamingCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GamingCard
